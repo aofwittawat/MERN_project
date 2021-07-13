735436d69017cab3f86d0b4dd7eb8de93c4fe232
Wittawat Boonyanuwat <aofwittawat@gmail.com>
2021-07-13 21:37:41 +0700

    Delete user and profile done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -117,4 +117,22 @@ router.get('/user/:user_id', async (req, res) => {
         res.status(500).send('Server Error')
     }
 })
+
+//@route    DELETE api/Profile
+//@desc     delete profile, user and posts
+//@access   private
+router.delete('/', auth,async (req, res) => {
+    try {
+        //@todo - remove users posts
+
+        // Remove Profile
+        await Profile.findOneAndRemove({user: req.user.id})
+        // Remove User
+        await User.findOneAndRemove({_id: req.user.id})
+        res.json({msg:"User deleted"})
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server Error')
+    }
+})
 module.exports = router;
