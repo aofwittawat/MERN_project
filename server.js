738ffead13bfb530aa0bc14e28880e7f163cde10
aofwittawat <aofwittawat@gmail.com>
2021-07-17 18:47:37 +0700

    heroku deploy prepairing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const cors = require('cors')
+const path = require('path')
 
 const corsOptions = {
     origin: ['http://localhost:3000', 'http://localhost:5000'],
@@ -16,13 +17,22 @@ connectDB()
 //Init Middleware
 app.use(express.json({ extended: false }))
 
-app.get('/', (req, res) => res.send('API running'))
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+//serve static assests in production
+if(process.env.NODE_ENV === 'production'){
+  // Set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 
 const PORT = process.env.PORT || 5000
 
